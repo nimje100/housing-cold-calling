9e8d9a747939fdfd156b4af808dd9c22ef7d21bf
Himanshu Nimje <himanshunimje@Himanshus-MacBook-Pro.local>
2024-08-28 09:01:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react";
+
+import ActiveCallDetail from "./components/ActiveCallDetail";
+import Button from "./components/base/Button";
+import Vapi from "@vapi-ai/web";
+import { isPublicKeyMissingError } from "./utils";
+
+// Put your Vapi Public Key below.
+const vapi = new Vapi("ae2bfc9f-1cba-417b-9783-fd0536f228f7");
+
+const App = () => {
+  const [connecting, setConnecting] = useState(false);
+  const [connected, setConnected] = useState(false);
+
+  const [assistantIsSpeaking, setAssistantIsSpeaking] = useState(false);
+  const [volumeLevel, setVolumeLevel] = useState(0);
+
+  const { showPublicKeyInvalidMessage, setShowPublicKeyInvalidMessage } = usePublicKeyInvalid();
+
+  // hook into Vapi events
+  useEffect(() => {
+    vapi.on("call-start", () => {
+      setConnecting(false);
+      setConnected(true);
+
+      setShowPublicKeyInvalidMessage(false);
+    });
+
+    vapi.on("call-end", () => {
+      setConnecting(false);
+      setConnected(false);
+
+      setShowPublicKeyInvalidMessage(false);
+    });
+
+    vapi.on("speech-start", () => {
+      setAssistantIsSpeaking(true);
+    });
+
+    vapi.on("speech-end", () => {
+      setAssistantIsSpeaking(false);
+    });
+
+    vapi.on("volume-level", (level) => {
+      setVolumeLevel(level);
+    });
+
+    vapi.on("error", (error) => {
+      console.error(error);
+
+      setConnecting(false);
+      if (isPublicKeyMissingError({ vapiError: error })) {
+        setShowPublicKeyInvalidMessage(true);
+      }
+    });
+
+    // we only want this to fire on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // call start handler
+  const startCallInline = () => {
+    setConnecting(true);
+    vapi.start("deaa8946-9c01-4a8c-a917-0243d2af3bc2");
+  };
+  const endCall = () => {
+    vapi.stop();
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {!connected ? (
+        <Button
+          label="Call"
+          onClick={startCallInline}
+          isLoading={connecting}
+        />
+      ) : (
+        <ActiveCallDetail
+          assistantIsSpeaking={assistantIsSpeaking}
+          volumeLevel={volumeLevel}
+          onEndCallClick={endCall}
+        />
+      )}
+    </div>
+  );
+};
+
+const usePublicKeyInvalid = () => {
+  const [showPublicKeyInvalidMessage, setShowPublicKeyInvalidMessage] = useState(false);
+
+  // close public key invalid message after delay
+  useEffect(() => {
+    if (showPublicKeyInvalidMessage) {
+      setTimeout(() => {
+        setShowPublicKeyInvalidMessage(false);
+      }, 3000);
+    }
+  }, [showPublicKeyInvalidMessage]);
+
+  return {
+    showPublicKeyInvalidMessage,
+    setShowPublicKeyInvalidMessage,
+  };
+};
+
+export default App;
